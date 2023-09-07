e723bb2143bb1d4c4109bf5f2eb1af3058810efe
Maximilian Stolzle <maximilian@stoelzle.ch>
2023-09-07 12:56:53 +0000

    Randomly sample bending setpoints

--- hsa_trajectory_planner/hsa_trajectory_planner/planar_bending_trajectory_node.py
+++ hsa_trajectory_planner/hsa_trajectory_planner/planar_bending_trajectory_node.py
@@ -3,7 +3,7 @@ from jax import config as jax_config
 
 jax_config.update("jax_enable_x64", True)  # double precision
 jax_config.update("jax_platform_name", "cpu")  # use CPU
-from jax import Array, jit
+from jax import Array, jit, random
 from jax import numpy as jnp
 import rclpy
 from rclpy.node import Node
@@ -20,6 +20,9 @@ from jsrm.systems import planar_hsa
 class PlanarBendingTrajectoryNode(Node):
     def __init__(self):
         super().__init__("planar_bending_trajectory_node")
+
+        # set random seed
+        self.rng = random.PRNGKey(seed=0)
 
         self.declare_parameter("setpoint_topic", "setpoint")
         self.setpoint_pub = self.create_publisher(
@@ -50,25 +53,35 @@ class PlanarBendingTrajectoryNode(Node):
             self.params["sigma_a_eq"]
         )
 
-        self.declare_parameter("phi_max", self.params["phi_max"].mean().item())
-        self.params["phi_max"] = self.get_parameter("phi_max").value * jnp.ones_like(
-            self.params["phi_max"]
-        )
+        # set the desired axial strain [-]
+        self.declare_parameter("sigma_a_des", 0.2876)
+        self.sigma_a_des = self.get_parameter("sigma_a_des").value  # rad/m
 
-        # for now, we just set a constant setpoint
-        self.declare_parameter("kappa_b_des", 7)
-        self.kappa_b_des = self.get_parameter("kappa_b_des").value  # rad/m
+        # set maximum value for the curvature [rad/m]
+        self.declare_parameter("kappa_b_max", 8)
+        self.kappa_b_max = self.get_parameter("kappa_b_max").value
 
         # initial setpoint index
         self.setpoint_idx = 0
 
-        self.declare_parameter("planning_frequency", 5)
+        self.declare_parameter(
+            "planning_frequency", 0.04
+        )  # a period of 25s between setpoints
         self.timer = self.create_timer(
             1 / self.get_parameter("planning_frequency").value, self.timer_callback
         )
 
     def timer_callback(self):
-        q_des = jnp.array([self.kappa_b_des, 0.0, 0.2876])
+        # split PRNG key
+        self.rng, rng_setpoint = random.split(self.rng)
+
+        # sample random desired curvature
+        kappa_b_des = random.uniform(
+            rng_setpoint, shape=(), minval=-self.kappa_b_max, maxval=self.kappa_b_max
+        )
+
+        # define the desired configuration
+        q_des = jnp.array([kappa_b_des, 0.0, self.sigma_a_des])
 
         # compute desired end effector pose with forward kinematics
         chiee_des = self.forward_kinematics_end_effector_fn(self.params, q_des)
