e9cc55fea508af927b28655f2b2f6b2b415eebe2
Maximilian Stolzle <maximilian@stoelzle.ch>
2023-10-05 14:57:36 +0000

    Add launch file for performing brain control on hsa robots

---
+++ launch/planar_hsa_robot_brain_control_launch.py
@@ -0,0 +1,91 @@
+# Planar HSA control launch file
+from ament_index_python.packages import get_package_share_directory
+from datetime import datetime
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess, TimerAction
+from launch_ros.actions import Node
+import numpy as np
+import os
+
+# datetime object containing current date and time
+now = datetime.now()
+
+RECORD_BAG = True  # Record data to rosbag file
+BAG_PATH = f"/home/mstoelzle/phd/rosbags/rosbag2_{now.strftime('%Y%m%d_%H%M%S')}"
+LOG_LEVEL = "warn"
+
+hsa_material = "fpu"
+kappa_b_eq = 0.0
+sigma_sh_eq = 0.0
+sigma_a_eq = 1.0
+
+if hsa_material == "fpu":
+    phi_max = 200 / 180 * np.pi
+elif hsa_material == "epu":
+    phi_max = 270 / 180 * np.pi
+else:
+    raise ValueError(f"Unknown HSA material: {hsa_material}")
+
+common_params = {
+    "hsa_material": hsa_material,
+    "kappa_b_eq": kappa_b_eq,
+    "sigma_sh_eq": sigma_sh_eq,
+    "sigma_a_eq": sigma_a_eq,
+    "phi_delta": np.pi / 250,  # step for each stimulation [rad]
+    "phi_max": phi_max,
+}
+planning_params = common_params | {
+    "planning_frequency": 0.025  # period of 40s between setpoints
+}
+viz_params = common_params | {
+    "rendering_frequency": 20.0,
+    "invert_colors": True
+}
+
+
+def generate_launch_description():
+    launch_actions = [
+        Node(
+            package="hsa_visualization",
+            executable="planar_viz_node",
+            name="visualization",
+            parameters=[viz_params],
+        ),
+        Node(
+            package="hsa_brain_control",
+            executable="planar_hsa_brain_control_node",
+            name="brain_control",
+            parameters=[common_params],
+            arguments=["--ros-args", "--log-level", LOG_LEVEL],
+        ),
+        Node(
+            package="hsa_actuation",
+            executable="hsa_planar_actuation_by_msg_node",
+            name="actuation",
+            parameters=[common_params],
+            arguments=["--ros-args", "--log-level", LOG_LEVEL],
+        ),
+        Node(
+            package="openvibe_bridge",
+            executable="stimulation_receiver_node",
+            name="brain_signal_publisher",
+            parameters=[{"host": "145.94.193.117"}],
+            arguments=["--ros-args", "--log-level", LOG_LEVEL],
+        ),
+        Node(
+            package="hsa_trajectory_planner",
+            executable="planar_bending_trajectory_node",
+            name="trajectory_planner",
+            parameters=[planning_params],
+            arguments=["--ros-args", "--log-level", LOG_LEVEL],
+        ),
+    ]
+
+    if RECORD_BAG:
+        launch_actions.append(
+            ExecuteProcess(
+                cmd=["ros2", "bag", "record", "-a", "-o", BAG_PATH], output="screen"
+            )
+        )
+
+    return LaunchDescription(launch_actions)
