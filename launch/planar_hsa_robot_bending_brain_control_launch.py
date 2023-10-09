e6dc3e75c0f49ccc242ae0646f5af68809d765bc
Maximilian Stolzle <maximilian@stoelzle.ch>
2023-10-09 14:27:34 +0000

    Rename launch files

--- launch/planar_hsa_robot_bending_brain_control_launch.py
+++ launch/planar_hsa_robot_bending_brain_control_launch.py
@@ -102,7 +102,7 @@ def generate_launch_description():
             package="openvibe_bridge",
             executable="stimulation_receiver_node",
             name="brain_signal_publisher",
-            parameters=[{"host": "145.94.193.117"}],
+            parameters=[{"host": "145.94.234.212"}],
             arguments=["--ros-args", "--log-level", LOG_LEVEL],
         ),
         Node(
